d96f1cf065d1157914c6077f9edc614d76248fb0
R. Miles McCain <x1-key@sendmiles.email>
2019-04-01 10:37:46 -0400

    Update text search

--- src/search_documents.py
+++ src/search_documents.py
@@ -16,10 +16,14 @@ def _split_into_chunks(text):
     MAX_ROW_LENGTH = 30000
     return [text[i:i+MAX_ROW_LENGTH] for i in range(0, len(text), MAX_ROW_LENGTH)]
 
+def _text(document):
+    return document["title"] + " " + document["text"]
+
 def is_match(document):
-    if "NATO" or "North Atlantic Treaty Organization" in document["text"]:
-        if _contains_any_term(["spending", "contribution", "percent of gdp", "defense budget"], document["text"].lower()): 
-            return True
+    if "nato" or "north atlantic treaty organization" in _text(document).lower():
+        if _contains_any_term(["spending", "contribution", "percent of gdp", "defense budget"], _text(document).lower()): 
+            if _contains_any_term(["north atlantic council", "north atlantic alliance"], _text(document).lower()):
+                return True
     return False
 
 matches = []
